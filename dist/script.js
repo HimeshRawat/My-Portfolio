2c0b8c95b84df371cdee60ff3ae45de2abe640bc
Abhishek Rawat <abrawatji66@outlook.com>
2024-08-14 00:11:17 +0530

    my portfolio

--- dist/script.js
+++ dist/script.js
@@ -1,16 +1,27 @@
+function Responsive() {
+  const MenuButton = document.querySelector(".menuButton");
+  const MenuBox = document.querySelector(".menuBox");
+  MenuButton.addEventListener("click", () => {
+    MenuBox.classList.toggle("hidden");
+  });
+}
+
 function connectButtonAndResumeButton() {
   document
     .getElementById("connectButton")
     .addEventListener("click", function () {
       window.location.href = "https://github.com/HimeshRawat";
     });
-    document
-    .getElementById("seeMore")
-    .addEventListener("click", function () {
-      window.location.href = "https://github.com/HimeshRawat";
-    });
+  document.getElementById("seeMore").addEventListener("click", function () {
+    window.location.href = "https://github.com/HimeshRawat";
+  });
   document
     .getElementById("resumeButton")
+    .addEventListener("click", function () {
+      window.open("Resume/Himesh CV.pdf", "_blank");
+    });
+  document
+    .getElementById("resume-Button")
     .addEventListener("click", function () {
       window.open("Resume/Himesh CV.pdf", "_blank");
     });
@@ -20,7 +31,6 @@ function SheryMagnetEffect() {
   Shery.makeMagnet(".right button");
   Shery.makeMagnet("#connectButton");
   Shery.makeMagnet("#seeMore");
-
 }
 function cursorAnimation() {
   document.addEventListener("mousemove", function (dets) {
@@ -30,14 +40,8 @@ function cursorAnimation() {
     });
   });
 }
-function locomotive(){
-  const scroll = new LocomotiveScroll({
-    el: document.querySelector(".main"),
-    smooth: true,
-  });
-  
-}
+
+Responsive();
 connectButtonAndResumeButton();
 SheryMagnetEffect();
 cursorAnimation();
-locomotive();
